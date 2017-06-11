b0dcfa4fea941da2df53793254b0ccc2b1a3af0e
Johan Swanepoel <johan.swanepoel@wyzetalk.com>
2017-06-11 18:13:13 +0200

    Added three main modules and basic functionality.

---
+++ app.js
@@ -0,0 +1,66 @@
+
+//  BUDGET CONTROLLER
+var budgetController = (function () {
+
+})();
+
+// UI CONTROLLER
+var UIController = (function () {
+    var DOMstrings = {
+        inputType: '.add__type',
+        addDescription: '.add__description',
+        addValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+    return {
+        getInput: function () {
+            return {
+                // instead of returning variables, return an object
+                type: document.querySelector(DOMstrings.inputType).value, // inc/exp
+                description: document.querySelector(DOMstrings.addDescription).value,
+                value: document.querySelector(DOMstrings.addValue).value
+            };
+
+        },
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    };
+
+})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function (budgetCtrl, UICtrl) {
+    var setupEventListeners = function () {
+        var DOM = UICtrl.getDOMstrings();
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+
+        });
+
+    };
+    var ctrlAddItem = function () {
+        // 1. Get field input data
+        var input = UICtrl.getInput();
+        console.log(input);
+
+        // 2. Add item to budget controller
+        // 3. Add item to the UI
+        // 4. Calculate the budget
+        // 5. Display the budget on the UI
+
+    };
+
+    return {
+        init: function(){
+            console.log('Application started successfully.');
+            setupEventListeners();
+        }
+    };
+
+})(budgetController, UIController);
+
+controller.init();
